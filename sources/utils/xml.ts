5c47b4a1b006324694ec17f5cfc5893b588f7675
piquark6046 <piquark6046@proton.me>
2024-05-26 04:28:04 +0000

    refactor: Remove XML validation and parsing functions
    The commit removes the ValidateXML and ParseXML functions from the utils/xml.ts file. These functions are no longer needed in the codebase and can be safely removed. This refactor simplifies the code and reduces unnecessary dependencies.

--- sources/utils/xml.ts
+++
@@ -1,9 +0,0 @@
-import * as FastXmlParser from 'fast-xml-parser'
-
-export function ValidateXML(XMLData: string) {
-  return FastXmlParser.XMLValidator.validate(XMLData)
-}
-
-export function ParseXML(XMLData: string) {
-  return new FastXmlParser.XMLParser().parse(XMLData)
-}
