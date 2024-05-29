cb086991c3280e41eafe140543a772817d67fe98
piquark6046 <piquark6046@proton.me>
2024-05-29 00:45:53 +0000

    fix: overflow issue while writing version data as cfb

--- sources/hwp/converter/cfb/fileheader.ts
+++ sources/hwp/converter/cfb/fileheader.ts
@@ -4,13 +4,18 @@ import { HwpFileHeader } from '../../container/fileheader.js'
 export function ConvertHwpFileHeaderToCfb(HwpFileHeaderInstance: HwpFileHeader): ArrayBuffer {
   const ByteWriterInstance = new ByteWriter(new ArrayBuffer(256))
   ByteWriterInstance.WriteString('HWP Document File') // Hwp File Signature
-  ByteWriterInstance.WriteUInt16(ConvertVersionToCfb(HwpFileHeaderInstance)) // Version
+  ByteWriterInstance.Write(ConvertVersionToCfb(HwpFileHeaderInstance)) // Version
   return ByteWriterInstance.ArrayBuffer()
 }
 
-function ConvertVersionToCfb(HwpFileHeaderInstance: HwpFileHeader): number {
-  return ((Number(HwpFileHeaderInstance.Version.split('.')[0]) & 0xff) << 24)
+function ConvertVersionToCfb(HwpFileHeaderInstance: HwpFileHeader): ArrayBuffer {
+  const ByteWriterInstance = new ByteWriter(new ArrayBuffer(4))
+  const VersionNumber = ((Number(HwpFileHeaderInstance.Version.split('.')[0]) & 0xff) << 24)
     | ((Number(HwpFileHeaderInstance.Version.split('.')[1]) & 0xff) << 16)
     | ((Number(HwpFileHeaderInstance.Version.split('.')[2]) & 0xff) << 8)
     | (Number(HwpFileHeaderInstance.Version.split('.')[3]) & 0xff)
+  ByteWriterInstance.WriteUInt8(VersionNumber)
+  return ByteWriterInstance.ArrayBuffer()
+}
+
 }
