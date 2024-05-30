599a5b919cc14fdc86dacbdc9bfb9221c51662d4
piquark6046 <piquark6046@proton.me>
2024-05-30 16:15:46 +0000

    feat: add reading Cfb ArrayBuffer to HwpFileHeader

--- sources/hwp/converter/cfb/fileheader.ts
+++ sources/hwp/converter/cfb/fileheader.ts
@@ -1,5 +1,5 @@
-import { ByteWriter } from '../../../utils/bytemgr.js'
-import { BitWriter } from '../../../utils/bitmgr.js'
+import { ByteWriter, ByteReader } from '../../../utils/bytemgr.js'
+import { BitWriter, BitReader } from '../../../utils/bitmgr.js'
 import { HwpFileHeader } from '../../container/fileheader.js'
 
 const HwpFileHeaderFlags = Object.entries(new HwpFileHeader()).filter(([Key, Value]) => Key !== 'UUID' && typeof Value === 'boolean').map(([Key]) => Key)
@@ -34,4 +34,34 @@ function ConvertEncryptionVersionToCfb(HwpFileHeaderInstance: HwpFileHeader): Ar
   const ByteWriterInstance = new ByteWriter(new ArrayBuffer(4))
   ByteWriterInstance.WriteUInt16(HwpFileHeaderInstance.EncryptionVersion)
   return ByteWriterInstance.ArrayBuffer()
+}
+
+export function ConvertCfbToHwpFileHeader(CfbArrayBuffer: ArrayBuffer): HwpFileHeader {
+  const ByteReaderInstance = new ByteReader(CfbArrayBuffer)
+  if (ByteReaderInstance.ArrayBuffer().byteLength !== 256) {
+    throw new Error(`ArrayBuffer for HwpFileHeader must be 256 bytes long. Actual: ${ByteReaderInstance.ArrayBuffer().byteLength}`)
+  }
+  if (new ByteReader(ByteReaderInstance.ArrayBuffer().slice(0, 17)).ReadString(Infinity) !== 'HWP Document File') {
+    throw new Error('Invalid Hwp File Signature')
+  }
+  const HwpFileHeaderInstance = new HwpFileHeader()
+  HwpFileHeaderInstance.Version = ConvertCfbToVersion(ByteReaderInstance.ArrayBuffer().slice(17, 21))
+  Object.entries(ConvertCfbToFlags(ByteReaderInstance.ArrayBuffer().slice(21, 25)))
+    .filter(([Key, Value]) => Key !== 'UUID' && typeof Value === 'boolean' && Value === true).map(([Key]) => Key)
+    .forEach(Flag => HwpFileHeaderInstance[Flag] = true)
+  HwpFileHeaderInstance.EncryptionVersion = new ByteReader(ByteReaderInstance.ArrayBuffer().slice(29, 31)).ReadUInt16() as 0 | 1 | 2
+  return HwpFileHeaderInstance
+}
+
+function ConvertCfbToVersion(CfbArrayBuffer: ArrayBuffer): string {
+  const ByteReaderInstance = new ByteReader(CfbArrayBuffer)
+  const VersionNumber = ByteReaderInstance.ReadUInt32()
+  return `${(VersionNumber >>> 24) & 0xff}.${(VersionNumber >>> 16) & 0xff}.${(VersionNumber >>> 8) & 0xff}.${VersionNumber & 0xff}`
+}
+
+function ConvertCfbToFlags(CfbArrayBuffer: ArrayBuffer): HwpFileHeader {
+  const BitReaderInstance = new BitReader(CfbArrayBuffer)
+  const HwpFileHeaderInstance = new HwpFileHeader()
+  HwpFileHeaderFlags.forEach(Flag => HwpFileHeaderInstance[Flag] = BitReaderInstance.ReadBit())
+  return HwpFileHeaderInstance
 }
